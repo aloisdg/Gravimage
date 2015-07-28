a88cf3a959b0790e869c801587e861ae8b2a498b
Tony <anovitsky@gmail.com>
2015-07-28 12:46:45 +0300

    added default image, + tests

---
+++ Gravimage.Tests/GravimageTest.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Net;
+using System.Net.Mime;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Gravimage.Tests
+{
+    [TestClass]
+    public class GravimageTest
+    {
+        [TestMethod]
+        public void GetDefault()
+        {
+            Gravimage.Get("test@test.com").Should().NotBeEmpty();
+        }
+
+        [TestMethod]
+        public void GetWithSize()
+        {
+            Gravimage.Get("test@test.com", 140).Should().Contain("s=140");
+        }
+
+        [TestMethod]
+        public void GetWithRatingType()
+        {
+            Gravimage.Get("test@test.com", 80, Gravimage.RatingType.R).Should().Contain("r=r");
+        }
+
+        [TestMethod]
+        public void GetWithDefaultImage()
+        {
+            Gravimage.Get("test@test.com", 80, Gravimage.RatingType.R, Gravimage.DefaultImage.Monsterid).Should().Contain("d=monsterid");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void GetWithNull()
+        {
+            Gravimage.Get(null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void GetWithBigSize()
+        {
+            Gravimage.Get("test", 2050);
+        }
+
+        [TestMethod]
+        public void CheckImageFromGravatar()
+        {
+            using (var client = new WebClient())
+            {
+                using (var strm = new MemoryStream(client.DownloadData(Gravimage.Get("test@test.com"))))
+                {
+                    var img = Image.FromStream(strm);
+                    img.Width.Should().Be(80);
+                    img.Height.Should().Be(80);
+                }
+            }
+        }
+    }
+}
--- Gravimage/Gravimage.cs
+++ Gravimage/Gravimage.cs
@@ -1,33 +1,87 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
 
 namespace Gravimage
 {
-    // https://secure.gravatar.com/site/implement/images/
+    /// <summary>
+    /// Implement https://secure.gravatar.com/site/implement/images/
+    /// </summary>
     public static class Gravimage
     {
-        private const string Url = "https://secure.gravatar.com/avatar.php?gravatar_id=";
+        private const string Url = "https://secure.gravatar.com/avatar.php";
 
         public enum RatingType
         {
+            /// <summary>
+            /// suitable for display on all websites with any audience type
+            /// </summary>
             G,
+            /// <summary>
+            /// may contain rude gestures, provocatively dressed individuals, the lesser swear words, or mild violence.
+            /// </summary>
             PG,
+            /// <summary>
+            /// may contain such things as harsh profanity, intense violence, nudity, or hard drug use.
+            /// </summary>
             R,
+            /// <summary>
+            /// may contain hardcore sexual imagery or extremely disturbing violence.
+            /// </summary>
             X
         }
 
-        // http://stackoverflow.com/q/424366
-        public static string Get(string email, int size = 80, RatingType ratingType = RatingType.PG)
+        public enum DefaultImage
         {
+            /// <summary>
+            /// (mystery-man) a simple, cartoon-style silhouetted outline of a person (does not vary by email hash)
+            /// </summary>
+            MM,
+            /// <summary>
+            ///  a geometric pattern based on an email hash
+            /// </summary>
+            Identicon,
+            /// <summary>
+            /// a generated 'monster' with different colors, faces, etc
+            /// </summary>
+            Monsterid,
+            /// <summary>
+            /// generated faces with differing features and backgrounds
+            /// </summary>
+            Wavatar,
+            /// <summary>
+            /// awesome generated, 8-bit arcade-style pixelated faces
+            /// </summary>
+            Retro,
+            /// <summary>
+            /// a transparent PNG image (border added to HTML below for demonstration purposes)
+            /// </summary>
+            Blank
+        }
+
+        // http://stackoverflow.com/q/424366
+        /// <summary>
+        /// Gets the link to avatar by gravatar.
+        /// </summary>
+        /// <param name="content">The content (email).</param>
+        /// <param name="size">The size of avatar.</param>
+        /// <param name="ratingType">Type of the rating.</param>
+        /// <param name="defaultImage">The type of default image.</param>
+        /// <returns></returns>
+        /// <exception cref="System.ArgumentNullException">content</exception>
+        /// <exception cref="System.ArgumentOutOfRangeException">size;The image size should be between 1 and 2048</exception>
+        public static string Get(string content, int size = 80, RatingType ratingType = RatingType.PG, DefaultImage defaultImage = DefaultImage.MM)
+        {
+            if (content == null) throw new ArgumentNullException("content");
+
             if (size < 1 | size > 2048)
                 throw new ArgumentOutOfRangeException("size",
                     "The image size should be between 1 and 2048");
 
-            return Url +  MD5.GetHashString(email.ToLower()).ToLower()
-                + "&s=" + size + "&r=" + Enum.GetName(typeof(RatingType), ratingType).ToLower() + "&d=mm";
+            return String.Format("{0}?gravatar_id={1}&s={2}&r={3}&d={4}",
+                Url,
+                MD5.GetHashString(content.ToLower()),
+                size,
+                ratingType.ToString("G"),
+                defaultImage.ToString("G")).ToLower();
         }
     }
 }
